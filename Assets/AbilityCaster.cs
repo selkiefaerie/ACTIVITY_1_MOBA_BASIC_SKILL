83ad2c1199c1cd2e90770ed87519df746f007b3f
Ma. Therese Andrea P. Carrillo <mathereseandreacarrillo0914@gmail.com>
2026-02-07 19:42:08 +0800

    Update AbilityCaster.cs

--- Assets/AbilityCaster.cs
+++ Assets/AbilityCaster.cs
@@ -18,15 +18,10 @@ public class AbilityCaster : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        // Perform the raycast
         if (Physics.Raycast(ray, out hit, 100f))
         {
-            // Debug line so you can see the ray in the Scene view while playing
-            Debug.DrawLine(ray.origin, hit.point, Color.cyan, 2f);
-
             if (hit.collider.CompareTag("Enemy"))
             {
-                // Check if this enemy already has a bomb
                 if (hit.transform.GetComponentInChildren<ExplosiveCharge>() != null)
                 {
                     Debug.Log("Target already has a bomb!");
@@ -46,6 +41,5 @@ public class AbilityCaster : MonoBehaviour
     {
         GameObject newBomb = Instantiate(bombPrefab, target.position, Quaternion.identity);
         newBomb.transform.SetParent(target);
-        // We removed the localPosition line here because your ExplosiveCharge script handles it now!
     }
 }
